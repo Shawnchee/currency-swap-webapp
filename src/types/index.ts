2d1de9ef181ea3d61d8a40e8d8ab66d753dea2dd
Shawn Chee <aykaychee2@gmail.com>
2026-02-05 16:11:31 +0800

    chore: fix file import and renaming v2

--- src/types/index.ts
+++
@@ -1,9 +0,0 @@
-export type CurrencyCode =
-    | 'USD' | 'HKD' | 'AUD' | 'MYR' | 'GBP'
-    | 'EUR' | 'IDR' | 'NZD' | 'CNY' | 'CZK' | 'AED';
-
-export interface Currency {
-    code: CurrencyCode;
-    name: string;
-    decimals: number;
-}
